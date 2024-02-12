c9a5743980e0c632e4f24a040e5b0f1f9c043ccf
Michael <limichael909@gmail.com>
2024-02-12 18:09:00 -0500

    give rec adjustments

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Box, Container, Text, Input, HStack, FormLabel, Icon, IconButton, useDisclosure } from '@chakra-ui/react';
 import { motion } from "framer-motion";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import ColorText from '../components/ColorText';
 import {
   Drawer,
@@ -21,27 +21,27 @@ import toast, { Toaster } from 'react-hot-toast';
 export default function Home() {
   const form = useRef();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [ disabled, setDisabled ] = useState(false);
 
   function sendEmail(e){
     e.preventDefault();
+    setDisabled(true);
 
     emailjs.sendForm('service_82pnpd8', 'template_2rs2ghj', form.current, 'rID95kHXL6tC-6Opp')
       .then((result) => {
         toast('Received, Thank you!', {
           icon: '💖',
         });
-        setTimeout(() => {
-          onClose();
-        }, 500)
-        
+        setDisabled(false);
+        onClose();
       }, (error) => {
           console.log(error.text);
+          setDisabled(false);
       });
   }
 
   return (
     <>
-    
         <Head>
           <title>Michael Li</title>
           <link rel="icon" href="/favicon.ico"></link>
@@ -60,13 +60,11 @@ export default function Home() {
             <form ref={form} onSubmit={sendEmail} className="font">
               <FormLabel color={"#fffbf2"}>Name</FormLabel>
               <Input type="text" name="user_name" defaultValue={"anonymous"} color={'#fffbf2'} borderColor={"#c2b199"} focusBorderColor='#c2bf99'/>
-              <FormLabel color={"#fffbf2"} mt={2}>Email</FormLabel>
-              <Input type="email" name="user_email" defaultValue={"limichael909@gmail.com"} color={'#fffbf2'} borderColor={"#c2b199"} focusBorderColor='#c2bf99'/>
               <FormLabel color={"#fffbf2"} mt={2}>Message</FormLabel>
               
               <HStack>
                 <Input name="message" placeholder={"the rec"} _placeholder={{ color: '#fffbf2' }} color={'#fffbf2'} borderColor={"#c2b199"} focusBorderColor='#c2bf99'/>
-                <IconButton type="submit" aria-label='Send Rec' icon={<GrSend />}  bgColor={"#fff7e4"} borderWidth={"1.2px"}/>
+                <IconButton disabled={disabled} type="submit" aria-label='Send Rec' icon={<GrSend />}  bgColor={"#fff7e4"} borderWidth={"1.2px"}/>
               </HStack>
               
             </form>
